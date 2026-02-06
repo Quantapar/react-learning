f792a99dcae66c8b08c657b0987b06f7abb4530e
Manu Sharma <manusharma2462@gmail.com>
2026-02-06 17:21:54 +0530

    react memo understanding

--- cohort-react/src/App.tsx
+++ cohort-react/src/App.tsx
@@ -1,39 +1,93 @@
+import React from "react";
 import { useState } from "react";
 
 type HeaderProps = {
   title: string | number;
 };
 
-function HeaderWithButton() {
-  const [firstTitle, setFirstTitle] = useState<string | number>("manu sharma");
+// normal way , both child of the componenet re render , even tho the prop of only one is changing
 
-  function updateMytitle() {
-    setFirstTitle(Math.random());
-  }
+// function Header({ title }: HeaderProps) {
+//   return (
+//     <>
+//       <div>my name is {title}</div>
+//     </>
+//   );
+// }
 
+// function App() {
+//   const [title, setTitle] = useState<string | number>("manu");
+//   function updateMytitle() {
+//     setTitle(Math.random());
+//   }
+//   return (
+//     <>
+//       <button onClick={updateMytitle}>update the title </button>
+//       <Header title={title} />
+//       <Header title={"manu"} />
+//     </>
+//   );
+// }
+
+// function HeaderWithButton() {
+//   const [firstTitle, setFirstTitle] = useState<string | number>("manu sharma");
+
+//   function updateMytitle() {
+//     setFirstTitle(Math.random());
+//   }
+
+//   return (
+//     <>
+//       <button onClick={updateMytitle}>Click me to change the title</button>
+//       <Header title={firstTitle} />
+//     </>
+//   );
+// }
+// function Header({ title }: HeaderProps) {
+//   return (
+//     <>
+//       <div>My name is {title}</div>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <HeaderWithButton />
+//       <Header title={"manu"} />
+//     </>
+//   );
+// }
+
+// this is how to optimize for re renders by splitting the components, the other way is
+//  We optimized re-renders by splitting state logic from display logic, so only the parts that depend on state need to re-render
+
+// now doing the same using react memo
+const Header = React.memo(function Header({ title }: HeaderProps) {
+  console.log("rerender");
   return (
     <>
-      <button onClick={updateMytitle}>Click me to change the title</button>
-      <Header title={firstTitle} />
+      <div>my name is {title}</div>
     </>
   );
-}
-
-function Header({ title }: HeaderProps) {
-  return (
-    <>
-      <div>My name is {title}</div>
-    </>
-  );
-}
+});
 
 function App() {
+  const [title, setTitle] = useState<string | number>("manu");
+  function updateMytitle() {
+    setTitle(Math.random());
+  }
   return (
     <>
-      <HeaderWithButton />
+      <button onClick={updateMytitle}>update the title </button>
+      <Header title={title} />
+      <Header title={"manu"} />
+      <Header title={"manu"} />
+      <Header title={"manu"} />
+      <Header title={"manu"} />
       <Header title={"manu"} />
     </>
   );
 }
-
 export default App;
