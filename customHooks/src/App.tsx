99c355fe57b320c0ef75238a16abeb38a7e0f76a
Manu Sharma <manusharma2462@gmail.com>
2026-02-18 22:52:47 +0530

    customHook

---
+++ customHooks/src/App.tsx
@@ -0,0 +1,8 @@
+// just like useState , useEffect , useCallback , we can write our own hooks
+// only naming convention is that their name should start with use
+
+export function App() {
+  return <></>;
+}
+
+export default App;
