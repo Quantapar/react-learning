1754324f579985b5cbed265954911a0a5d3c3726
Manu Sharma <manusharma2462@gmail.com>
2026-02-20 11:53:56 +0530

    assign

--- hooks-assign/src/App.tsx
+++ hooks-assign/src/App.tsx
@@ -1,5 +1,15 @@
+import { Assignment1 } from "components/Assignment1";
+import { Assignment2 } from "components/Assignment2";
+import { Assignment3 } from "components/Assignment3";
+
 export function App() {
-  return <></>;
+  return (
+    <>
+      <Assignment1 />
+      <Assignment2 />
+      <Assignment3 />
+    </>
+  );
 }
 
 export default App;
